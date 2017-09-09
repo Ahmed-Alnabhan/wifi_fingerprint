034c1b5b4b1faee8fbb5a120f28397c6ed18a31d
Ahmed-Alnabhan <amn5948@rit.edu>
2017-09-08 22:05:10 -0500

    feat: implement the wifi scan process

--- app/src/main/java/com/elearnna/www/wififingerprint/fragments/APsListFragment.java
+++ app/src/main/java/com/elearnna/www/wififingerprint/fragments/APsListFragment.java
@@ -1,19 +1,71 @@
 package com.elearnna.www.wififingerprint.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 import com.elearnna.www.wififingerprint.R;
+import com.elearnna.www.wififingerprint.model.AP;
+import com.elearnna.www.wififingerprint.presenter.APsListPresenter;
+import com.elearnna.www.wififingerprint.presenter.APsListPresenterImplementer;
+import com.elearnna.www.wififingerprint.view.APsListView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
-public class APsListFragment extends Fragment {
+public class APsListFragment extends Fragment implements APsListView{
 
+    APsListPresenter aPsListPresenter;
     public APsListFragment() {
         // Required empty public constructor
     }
+
+    @BindView(R.id.wifi_image)
+    ImageView wifiImage;
+
+    @BindView(R.id.tv_ssid)
+    TextView txtSSID;
+
+    @BindView(R.id.tv_ip_address)
+    TextView txtIPAddress;
+
+    @BindView(R.id.tv_connection_status)
+    TextView txtConnectionStatus;
+
+    @BindView(R.id.tv_channel)
+    TextView txtChennel;
+
+    @BindView(R.id.tv_mac_address)
+    TextView txtMAC;
+
+    @BindView(R.id.roundCornerProgressBar)
+    RoundCornerProgressBar roundCornerProgressBar;
+
+    @BindView(R.id.tv_signal_strength)
+    TextView txtSignalStrength;
+
+    @BindView(R.id.aps_loading_progress_bar)
+    ProgressBar apsLoadingProgressBar;
+
+    @BindView(R.id.rv_aps_list)
+    RecyclerView rvAPsList;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +76,69 @@ public class APsListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aps_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_aps_list, container, false);
+        ButterKnife.bind(this, view);
+        // request the user permission for location access
+        requestUserPermission();
+        rvAPsList.setLayoutManager(new LinearLayoutManager(getContext()));
+        aPsListPresenter = new APsListPresenterImplementer(getContext());
+        aPsListPresenter.setAPsListView(this);
+        aPsListPresenter.getAPsList();
+        return view;
     }
 
+    private void requestUserPermission() {
+        int myPermission = ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        if(!(myPermission == PackageManager.PERMISSION_GRANTED)){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            } else {
+                // do request the permission
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        10);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void displayAPsList(List<AP> APsList) {
+
+    }
+
+    @Override
+    public void showAPDetail(AP ap) {
+
+    }
+
+    @Override
+    public void displayErrorMessage() {
+
+    }
+
+    @Override
+    public void showAPsLoading() {
+
+    }
+
+    @Override
+    public void hideAPsLoading() {
+
+    }
 }
--- app/src/main/java/com/elearnna/www/wififingerprint/presenter/APsListPresenterImplementer.java
+++ app/src/main/java/com/elearnna/www/wififingerprint/presenter/APsListPresenterImplementer.java
@@ -1,8 +1,76 @@
 package com.elearnna.www.wififingerprint.presenter;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.util.Log;
+
+import com.elearnna.www.wififingerprint.view.APsListView;
+
+import java.util.List;
+
 /**
  * Created by Ahmed on 9/6/2017.
  */
 
-public class APsListPresenterImplementer {
+public class APsListPresenterImplementer implements APsListPresenter{
+
+    private APsListView aPsListView;
+    private WifiManager wifiManager;
+    private Handler handler;
+    private Runnable runnable;
+    private List<ScanResult> wifiAPsList;
+    private WiFiBroadcastReceiver wiFiBroadcastReceiver;
+    private Context mContext;
+    private IntentFilter intentFilter;
+
+    public APsListPresenterImplementer(Context context) {
+        this.mContext = context;
+    }
+
+    private void readWifiNetworks() {
+        wifiManager.setWifiEnabled(true);
+        wifiManager.startScan();
+        handler.postDelayed(runnable, 1000);
+    }
+
+    @Override
+    public void setAPsListView(APsListView view) {
+        aPsListView = view;
+    }
+
+    @Override
+    public void getAPsList() {
+        intentFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+        wiFiBroadcastReceiver = new WiFiBroadcastReceiver();
+        mContext.getApplicationContext().registerReceiver(wiFiBroadcastReceiver, intentFilter);
+        aPsListView.showAPsLoading();
+        handler = new Handler();
+        runnable = new Runnable(){
+            @Override
+            public void run() {
+                readWifiNetworks();
+            }
+        };
+        runnable.run();
+        if(!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+
+
+    }
+
+    private class WiFiBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            wifiAPsList = wifiManager.getScanResults();
+            Log.d("AAA", String.valueOf(wifiAPsList.size()));
+        }
+    }
 }
