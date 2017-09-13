18d2747ad5116248acccbaafe12cb78f024e641d
Ahmed-Alnabhan <amn5948@rit.edu>
2017-09-12 22:54:59 -0500

    feat: Create the drawer list items and icons

--- app/src/main/java/com/elearnna/www/wififingerprint/activities/APsList.java
+++ app/src/main/java/com/elearnna/www/wififingerprint/activities/APsList.java
@@ -82,17 +82,17 @@ public class APsList extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_chart) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_aps) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_files) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_device_info) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
