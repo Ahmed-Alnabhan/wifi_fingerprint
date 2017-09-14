a9ab1543915689e6d754a41bc037b7fd9137a752
Ahmed-Alnabhan <amn5948@rit.edu>
2017-09-13 22:06:05 -0500

    feat: Create and designthe APDetail activity and fragment

---
+++ app/src/main/java/com/elearnna/www/wififingerprint/activities/APDetail.java
@@ -0,0 +1,21 @@
+package com.elearnna.www.wififingerprint.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.elearnna.www.wififingerprint.R;
+import com.elearnna.www.wififingerprint.fragments.APDetailFragment;
+
+public class APDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apdetail);
+
+        APDetailFragment fragment = new APDetailFragment();
+        FragmentManager fragmentMananger = getSupportFragmentManager();
+        fragmentMananger.beginTransaction().add(R.id.fragment_ap_detail, fragment);
+    }
+}
