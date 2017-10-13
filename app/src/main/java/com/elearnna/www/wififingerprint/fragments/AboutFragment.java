b1c01a6c28bdf13bac0f46f7e2eda6f4a9b7ff7f
Ahmed-Alnabhan <amn5948@rit.edu>
2017-10-13 01:12:37 -0500

    feat: Apply a new theme to the about app gragment

--- app/src/main/java/com/elearnna/www/wififingerprint/fragments/AboutFragment.java
+++ app/src/main/java/com/elearnna/www/wififingerprint/fragments/AboutFragment.java
@@ -1,9 +1,7 @@
 package com.elearnna.www.wififingerprint.fragments;
 
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,8 +12,9 @@ import android.view.ViewGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.elearnna.www.wififingerprint.BuildConfig;
 import com.elearnna.www.wififingerprint.R;
-import com.elearnna.www.wififingerprint.app.Constants;
+import com.elearnna.www.wififingerprint.app.Utils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,19 +24,32 @@ import butterknife.ButterKnife;
  */
 public class AboutFragment extends Fragment {
 
-    private String aboutApp;
-    private String appVersion;
-    private Bundle savedInstance;
-    private int scrollState;
-
-    @BindView(R.id.txt_about_app)
-    TextView txtAboutApp;
-
-    @BindView(R.id.txt_app_version)
-    TextView txtAppVersion;
-
     @BindView(R.id.scroll_about_app_activity)
-    ScrollView scrollView;
+    ScrollView aboutScrollView;
+
+    @BindView(R.id.description_title)
+    TextView txtDescriptionTitle;
+
+    @BindView(R.id.description_body)
+    TextView txtDescriptionBody;
+
+    @BindView(R.id.icons_title)
+    TextView txtIconsTitle;
+
+    @BindView(R.id.icons_body)
+    TextView txtIconsBody;
+
+    @BindView(R.id.libraries_title)
+    TextView txtLibrariesTitle;
+
+    @BindView(R.id.libraries_body)
+    TextView txtLibrariesBody;
+    
+    @BindView(R.id.txt_app_version)
+    TextView  txtAppVersion;
+
+    private int scrollPositionX;
+    private int scrollPositionY;
 
     public AboutFragment() {
         // Required empty public constructor
@@ -50,23 +62,17 @@ public class AboutFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_about, container, false);
         ButterKnife.bind(this, view);
-        savedInstance = savedInstanceState;
-        // About app
-        aboutApp = Constants.ABOUT_APP;
-        if (Build.VERSION.SDK_INT >= 24){
-            txtAboutApp.setText(Html.fromHtml(aboutApp,Html.FROM_HTML_MODE_LEGACY));
-        } else {
-            txtAboutApp.setText(Html.fromHtml(Constants.ABOUT_APP));
-        }
 
-        // App's version
-        try {
-            PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
-            appVersion = pInfo.versionName;
-            txtAppVersion.setText("Version: " + appVersion);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
+        // Set the style of TextViews
+        setTextViewStyle();
+        
+        // Get app version
+        String appName = BuildConfig.VERSION_NAME;
+
+        txtAppVersion.setText("Version: " + appName);
+
+        txtLibrariesBody.setText(Html.fromHtml(getString(R.string.libraries_body)));
+        txtIconsBody.setText(Html.fromHtml(getString(R.string.icons_body)));
 
         return view;
     }
@@ -74,28 +80,62 @@ public class AboutFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if(savedInstance != null){
-            txtAppVersion.setText("");
-            txtAboutApp.setText("");
-        }
+
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
+        outState.putInt("scrollPositionX",aboutScrollView.getScrollX());
+        outState.putInt("scrollPositionY",aboutScrollView.getScrollY());
         super.onSaveInstanceState(outState);
-        outState.putString("aboutAPP", String.valueOf(txtAboutApp.getText()));
-        outState.putString("appVersion", String.valueOf(txtAppVersion.getText()));
-        outState.putInt("scrollState", scrollView.getScrollY());
+
     }
 
     @Override
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
-        if(savedInstanceState != null) {
-            aboutApp = savedInstanceState.getString("aboutAPP");
-            appVersion = savedInstanceState.getString("appVersion");
-
-            scrollView.setScrollY(savedInstanceState.getInt("scrollState"));
+        if (savedInstanceState != null) {
+            scrollPositionX = savedInstanceState.getInt("scrollPositionX");
+            scrollPositionY = savedInstanceState.getInt("scrollPositionY");
+            aboutScrollView.post(new Runnable() {
+                @Override
+                public void run() {
+                    aboutScrollView.scrollTo(scrollPositionX, scrollPositionY);
+                }
+            });
         }
+
+    }
+
+    /**
+     * This method sets style to the views
+     */
+
+    private void setTextViewStyle() {
+        // set quicksand bold font
+        Typeface bold_font = Utils.setQuicksandBoldFont(getContext());
+        // set quicksand regular font
+        Typeface regular_font = Utils.setQuicksandRegularFont(getContext());
+
+        // Set the style of the description title TextView
+        Utils.setTextViewStyle(getContext(), txtDescriptionTitle, regular_font, "Large");
+
+        // Set the style of the description body TextView
+        Utils.setTextViewStyle(getContext(), txtDescriptionBody, regular_font, "Regular");
+
+        // Set the style of the libraries title TextView
+        Utils.setTextViewStyle(getContext(), txtLibrariesTitle, regular_font, "Large");
+
+        // Set the style of the libraries body TextView
+        Utils.setTextViewStyle(getContext(), txtLibrariesBody, regular_font, "Regular");
+
+        // Set the style of the icons title TextView
+        Utils.setTextViewStyle(getContext(), txtIconsTitle, regular_font, "Large");
+
+        // Set the style of the icons body TextView
+        Utils.setTextViewStyle(getContext(), txtIconsBody, regular_font, "Regular");
+
+        // Set the style of the app version TextView
+        Utils.setTextViewStyle(getContext(), txtAppVersion, bold_font, "Large");
     }
 }
